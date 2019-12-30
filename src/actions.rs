dc196ee84d039c46e0180c9c2dea8d8a1b25ced6
Elton <eltonp3103@gmail.com>
2019-12-30 15:21:18 -0500

    moving display code temporarily to actions

--- src/actions.rs
+++ src/actions.rs
@@ -1,103 +1,54 @@
-use std::fmt;
-use std::error::Error;
-use std::convert::From;
-use serde::{Deserialize, Serialize};
+use crate::App;
+use std::io::{stdin, stdout, Write};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::{clear, color, cursor, style};
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Story {
-    by: String,
-    descendants: Option<i32>,
-    id: i32,
-    kids: Option<Vec<i32>>,
-    score: i32, 
-    title: String,
-    url: Option<String>
-}
+type RawTerminal = termion::raw::RawTerminal<std::io::Stdout>;
 
-#[derive(Debug)]
-pub struct FetcherError { 
-    cause: String
-}
-
-impl fmt::Display for FetcherError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Fetcher failed to fetch data from hackernews")
-    }
-}
-
-impl Error for FetcherError { }
-
-impl From<reqwest::Error> for FetcherError {
-    fn from(_error: reqwest::Error) -> Self {
-        FetcherError { cause: String::from("something went wrong") }
-    }
-}
-
-pub struct Fetcher {
-    base_url: String,
-}
-
-impl Fetcher {
-    pub fn new(base_url: String) -> Fetcher {
-        Fetcher {
-            base_url
-        }
-    }
-
-    pub fn fetch_stories(&self) -> Result<Vec<Story>, FetcherError> {
-        let url = format!("{}{}", self.base_url, "topstories.json");
-        
-        let body = reqwest::get(url.as_str())?
-            .text()?;
-        
-        let ids = Fetcher::parse_top_stories_body(&body);
-        let stories = self.get_stories(ids);
-        Ok(stories)
-    }
-
-    pub fn fetch_story(&self, id: &str) -> Result<Story, FetcherError> {
-        let url = format!("{}{}{}{}", self.base_url, "item/", id, ".json");
-        let body = reqwest::get(url.as_str())?
-            .text()?;
-        let story: Story = serde_json::from_str(body.as_str()).unwrap();
-
-        Ok(story)
-    }
-
-    fn parse_top_stories_body(body: &str) -> Vec<String> {
-        let mut result = Vec::new();
-        let mut it = body.trim().chars();
-        
-        if it.next().unwrap() != '[' {
-            panic!("Invalid body received");
-        }
-
-        loop {
-            let mut ch = it.next().unwrap();
-            if ch.is_digit(10) {
-                let mut id = String::new();
-                while ch != ',' && ch != ']' {
-                    id.push(ch);
-                    ch = it.next().unwrap();
-                }
-                result.push(id);
-            }
-            if ch == ']' {
+pub fn processKeyPress(stdout: &mut RawTerminal, app: &mut App) {
+    for c in stdin().keys() {
+        match c.unwrap() {
+            Key::Char('q') => {
+                clear(stdout);
                 break;
             }
+            Key::Char('j') | Key::Down => {
+                if app.current_story_index < app.stories.len() {
+                    app.cursor_y += 4;
+                    app.current_story_index += 1;
+                }
+            }
+            Key::Char('k') | Key::Up => {
+                if app.current_story_index > 0 {
+                    app.cursor_y -= 4;
+                    app.current_story_index -= 1;
+                }
+            }
+            Key::Char('s') => {
+
+            }
+            _ => continue,
         }
-
-        result
-    }
-
-    fn get_stories(&self, ids: Vec<String>) -> Vec<Story> {
-        let mut stories: Vec<Story> = Vec::new();
-        
-        for id in ids {
-            let story = self.fetch_story(&id).unwrap();
-            stories.push(story);
-        }
-
-        stories
+        display(stdout, &app);
     }
 }
+
+fn clear(stdout: &mut RawTerminal) {
+    writeln!(
+        stdout,
+        "{clear}{goto}",
+        clear = clear::All,
+        goto = cursor::Goto(1, 1)
+    ).unwrap()
+}
+
+pub fn display(stdout: &mut RawTerminal, app: &App) {
+    // takes in stdout, and state
+    // displays it based on flags
+    clear(stdout);
+    writeln!(stdout, "x:{}, y:{}", app.cursor_x, app.cursor_y).unwrap();
+    writeln!(stdout, "Testing").unwrap();
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,22 @@
 mod app;
 mod fetcher;
 mod view;
+mod actions;
 
 use app::App;
-use std::io::{stdin, stdout, Write};
-use termion::event::Key;
-use termion::input::TermRead;
+use actions::*;
+use std::io::stdout;
 use termion::raw::IntoRawMode;
-use termion::{clear, color, cursor, style};
 
 type RawTerminal = termion::raw::RawTerminal<std::io::Stdout>; 
 
 fn main() {
     let stdout = &mut enter_raw_mode(); // getting stdout for display function
 
-    let app = App::init(); // initializing app
+    let mut app = App::init(); // initializing app
 
     display(stdout, &app);
-
-    for c in stdin().keys() {
-        match c.unwrap() {
-            Key::Char('q') => {
-                clear(stdout);
-                break;
-            }
-            _ => continue,
-        }
-    }
-}
-
-fn clear(stdout: &mut RawTerminal) {
-    writeln!(
-        stdout,
-        "{clear}{goto}",
-        clear = clear::All,
-        goto = cursor::Goto(1, 1)
-    ).unwrap()
-}
-
-fn display(stdout: &mut RawTerminal, app: &App) {
-    // takes in stdout, and state
-    // displays it based on flags
-    writeln!(stdout, "Testing").unwrap();
+    processKeyPress(stdout, &mut app);
 }
 
 fn enter_raw_mode() -> RawTerminal {
